d69c57695acda1c44d87134043a6eafa516d699e
Muhammad Shehroz Sarmad <shehrozm107@gmail.com>
2024-09-08 01:11:50 +0500

    worked on authentication with separation of concerns

---
+++ controllers/authController.js
@@ -0,0 +1,43 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const { generateToken } = require("../utils/generateToken");
+
+module.exports.registerUser = async (req, res) => {
+  try {
+    let { fullname, email, password } = req.body;
+
+    const usr = await userModel.findOne({ email });
+    if (usr)
+      return res
+        .status(401)
+        .send("you already have an account, try logging in");
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) return res.send(err.message);
+        else {
+          const user = await userModel.create({ fullname, email, password: hash });
+          res.cookie("token", generateToken(user));
+          res.send("user created successfuly");
+        }
+      });
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+module.exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) return res.send("user not found");
+  bcrypt.compare(password, user.password, (err, result) => {
+    if(result){
+        res.cookie('token', generateToken(user));
+        res.send('you are logged in ✅');
+    }else{
+        res.send('invalid cridentials ❌');
+    }
+  });
+};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,31 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const userModel = require("../models/user.model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const { registerUser, loginUser} = require('../controllers/authController');
 
 router.get("/", (req, res) => {
   res.send("hy working!");
 });
 
-router.post("/register", async (req, res) => {
-  try {
-    let { fullname, email, password } = req.body;
+router.post("/register", registerUser);
 
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        if (err) return res.send(err.message);
-        else {
-          const user = await userModel.create({ fullname, email, password });
-          const token = jwt.sign({email, id: user._id}, 'secretkey');
-          res.cookie("token", token);
-          res.send('user created successfuly');
-        }
-      });
-    });
-  } catch (error) {
-    res.send(error.message);
-  }
-});
+router.post('/login', loginUser);
 
 module.exports = router;
